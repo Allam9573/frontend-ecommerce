a4a6e5290627f5c0e82e16088544e99c8987ed65
Allam9573 <aallamdaniel@gmail.com>
2023-12-18 16:24:41 -0600

    segundo

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -38,7 +38,8 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form action="">
+                    <form action="{{ route('crearProducto') }}" method="POST">
+                        @csrf
                         <input type="text" placeholder="Nombre de producto" name="nombre" id=""
                             class="form-control mb-2">
                         <input type="text" placeholder="Precio Lp. 100.00" name="precio" id=""
